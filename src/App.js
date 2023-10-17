0da462c44f1857c3923487f6701e3faa9091690d
Kanagaraj-R <kanagarajram88@gmail.com>
2023-10-17 22:23:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TickTacToe from './Components/ticktactoe/TickTacToe'
+
+const App = () => {
+  return (
+    <div>
+      <TickTacToe/>
+    </div>
+  )
+}
+
+export default App
